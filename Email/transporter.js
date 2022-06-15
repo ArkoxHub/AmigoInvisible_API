fd2009e79b5161aa033bb2b0834e4f6a714430eb
Adrià Gual <agualandreu@gmail.com>
2022-06-16 01:42:45 +0200

    Dependencies

--- Email/transporter.js
+++ Email/transporter.js
@@ -2,11 +2,11 @@ const nodemailer = require('nodemailer');
 
 let transporter = nodemailer.createTransport({
     // Create a SMTP transporter object
-    host: 'smtp.gmail.com',
+    host: process.env.HOST,
     port: 587,
     auth: {
-        user: 'sorteo@amigoinvisible.net',
-        pass: 'ipgglwfqastjpbat'
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
     },
     tls: {
         rejectUnauthorized: false
