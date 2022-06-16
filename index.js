02bc00d3dbfa8df2de2459618f777b7e12b23307
Adrià Gual <agualandreu@gmail.com>
2022-06-17 01:53:05 +0200

    DOTENV

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ const connection = database;
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect(connection);
-    console.log(`Data Base Connected! \nConnection => ${database}`);
+    await mongoose
+        .connect(connection, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log(`Data Base Connected! \nConnection => ${database}`))
+        .catch(err => console.log(err));
 
     app.listen(port, () => {
         console.log(`Server is up and listening at http://localhost:${port}`);
